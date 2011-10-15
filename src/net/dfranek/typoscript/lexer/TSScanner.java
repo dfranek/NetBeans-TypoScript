267f3fc8a99dc2fb9f21c6be0b979e7df4c37bad
Daniel Franek <daniel@dfrank.net>
2011-10-15 13:49:06 +0200

    Highlighting of Keywords and reserved words, added GPL-Licence.

--- src/net/dfranek/typoscript/lexer/TSScanner.java
+++ src/net/dfranek/typoscript/lexer/TSScanner.java
@@ -52,222 +52,231 @@ import org.netbeans.spi.lexer.LexerRestartInfo;
  */
 public class TSScanner {
 
-    /* user code: */
-    private LexerInput input;
-    private boolean inValue = false;
-    private boolean inComment = false;
-    private boolean regexp = false;
-    
-    private int readLength = 0;
+	/* user code: */
+	private LexerInput input;
+	private boolean inValue = false;
+	private boolean inComment = false;
+	private boolean regexp = false;
+	private int readLength = 0;
 
-    public TSScanner(LexerRestartInfo info) {
-        this.input = info.input();
-    }
+	public TSScanner(LexerRestartInfo info) {
+		this.input = info.input();
+	}
 
-    /**
-     * Resumes scanning until the next regular expression is matched,
-     * the end of input is encountered or an I/O-Error occurs.
-     *
-     * @return      the next token
-     * @exception   java.io.IOException  if any I/O-Error occurs
-     */
-    public TSTokenId nextToken() throws java.io.IOException {
-        TSTokenId token = TSTokenId.UNKNOWN_TOKEN;
-        
-        //input.backup(1);
-        char ch = (char) input.read();
+	/**
+	 * Resumes scanning until the next regular expression is matched,
+	 * the end of input is encountered or an I/O-Error occurs.
+	 *
+	 * @return      the next token
+	 * @exception   java.io.IOException  if any I/O-Error occurs
+	 */
+	public TSTokenId nextToken() throws java.io.IOException {
+		TSTokenId token = TSTokenId.UNKNOWN_TOKEN;
+
+		//input.backup(1);
+		char ch = (char) input.read();
 
 		if (ch == '\n') {
-            token = TSTokenId.TS_NL;
-            inValue = false;
-        } else if (!this.inValue && this.inComment) {
-            token = readMultilineComment(ch);
-        } else if (isWhiteSpace(ch)) {
-            nextWhileWhiteSpace();
-            token = TSTokenId.WHITESPACE;
-        } else if (!this.inValue && (ch == '"' || ch == '\'')) {
-            nextUntilUnescaped(ch);
-            token = TSTokenId.TS_STRING;
-        } else if ((ch == '<' || ch == '>' || (ch == '=' && (char) input.read() != '<')) && (char) input.read() != '\n' && !this.inValue) { // there must be some value behind the operator!
-            this.inValue = true;
-            token = TSTokenId.TS_OPERATOR;
-            input.backup(1);
-            if(ch == '=') input.backup(1);
-        } else if (!this.inValue && ch == '[') {
-            nextUntilUnescaped(']');
-            token = TSTokenId.TS_CONDITION;
-            // with punctuation, the type of the token is the symbol itself
-        } else if (!this.inValue && Pattern.matches("[\\[\\]\\(\\),;\\:\\.\\<\\>\\=]", new Character(ch).toString())) {
-            token = TSTokenId.TS_OPERATOR;
-        } else if (!this.inValue && (ch == '{' || ch == '}')) {
-            token = TSTokenId.TS_CURLY;
-        } else if (!this.inValue && ch == '0' && (input.read() == 'x' || input.read() == 'X')) {
-            token = readHexNumber();
-        } else if (!this.inValue && isDigit(new Character(ch).toString())) {
-            token = readNumber();
-        } else if (!this.inValue && ch == '/') {
-            char next = (char) input.read();
+			token = TSTokenId.TS_NL;
+			inValue = false;
+		} else if (!this.inValue && this.inComment) {
+			token = readMultilineComment(ch);
+		} else if (isWhiteSpace(ch)) {
+			nextWhileWhiteSpace();
+			token = TSTokenId.WHITESPACE;
+		} else if (!this.inValue && (ch == '"' || ch == '\'')) {
+			nextUntilUnescaped(ch);
+			token = TSTokenId.TS_STRING;
+		} else if ((ch == '<' || ch == '>' || (ch == '=' && (char) input.read() != '<')) && (char) input.read() != '\n' && !this.inValue) { // there must be some value behind the operator!
+			this.inValue = true;
+			token = TSTokenId.TS_OPERATOR;
+			input.backup(1);
+			if (ch == '=') {
+				input.backup(1);
+			}
+		} else if (!this.inValue && ch == '[') {
+			nextUntilUnescaped(']');
+			token = TSTokenId.TS_CONDITION;
+			// with punctuation, the type of the token is the symbol itself
+		} else if (!this.inValue && Pattern.matches("[\\[\\]\\(\\),;\\:\\.\\<\\>\\=]", new Character(ch).toString())) {
+			token = TSTokenId.TS_OPERATOR;
+		} else if (!this.inValue && (ch == '{' || ch == '}')) {
+			token = TSTokenId.TS_CURLY;
+		} else if (!this.inValue && ch == '0' && (input.read() == 'x' || input.read() == 'X')) {
+			token = readHexNumber();
+		} else if (!this.inValue && isDigit(new Character(ch).toString())) {
+			token = readNumber();
+		} else if (!this.inValue && ch == '/') {
+			char next = (char) input.read();
 
-            if (next == '*') {
-                token = readMultilineComment(ch);
+			if (next == '*') {
+				token = readMultilineComment(ch);
 
-            } else if (next == '/') {
-                nextUntilUnescaped('\n');
-                token = TSTokenId.TS_COMMENT;
+			} else if (next == '/') {
+				nextUntilUnescaped('\n');
+				token = TSTokenId.TS_COMMENT;
 
-            } else if (this.regexp) {
-                token = readRegexp();
+			} else if (this.regexp) {
+				token = readRegexp();
 
-            } else {
-                nextWhileOperatorChar();
-                token = TSTokenId.TS_OPERATOR;
-            }
+			} else {
+				nextWhileOperatorChar();
+				token = TSTokenId.TS_OPERATOR;
+			}
 
-        } else if (!this.inValue && ch == '#') {
-            nextUntilUnescaped('\n');
-            token = TSTokenId.TS_COMMENT;
-        } else if (!this.inValue && isOperatorChar(new Character(ch).toString())) {
-            nextWhileOperatorChar();
-            token = TSTokenId.TS_OPERATOR;
-        } else {
-            nextWhileWordChar();
-            if (inValue) {
-                token = TSTokenId.TS_VALUE;
-            } else {
-                token = TSTokenId.TS_PROPERTY;
-            }
-        }
-        
-        this.readLength = input.readLength();
-        return token;
-    }
+		} else if (!this.inValue && ch == '#') {
+			nextUntilUnescaped('\n');
+			token = TSTokenId.TS_COMMENT;
+		} else if (!this.inValue && isOperatorChar(new Character(ch).toString())) {
+			nextWhileOperatorChar();
+			token = TSTokenId.TS_OPERATOR;
+		} else {
+			String word = nextWhileWordChar();
+			if (Pattern.matches(TSScannerKeyWords.keywords, word)) {
+				token = TSTokenId.TS_KEYWORD;
+			} else if(Pattern.matches(TSScannerKeyWords.keywords2, word)) {
+				token = TSTokenId.TS_KEYWORD2;
+			} else if(Pattern.matches(TSScannerKeyWords.keywords3, word)) {
+				token = TSTokenId.TS_KEYWORD3;
+			} else if(Pattern.matches(TSScannerKeyWords.reservedWord, word)) {
+				token = TSTokenId.TS_RESERVED;
+			} else if (inValue) {
+				token = TSTokenId.TS_VALUE;
+			} else {
+				token = TSTokenId.TS_PROPERTY;
+			}
+		}
 
-    protected TSTokenId readRegexp() {
-        nextUntilUnescaped('/');
-        nextWhileMatchesRegExp("[gi]");
-        return TSTokenId.TS_REGEXP;
-    }
+		this.readLength = input.readLength();
+		return token;
+	}
 
-    protected void nextWhileWordChar(){
-        StringBuilder s = new StringBuilder();
-        char next;
-        while (((next = (char) input.read()) != LexerInput.EOF) && isWordChar(new Character(next).toString()) && next != '\n') {
-            s.append(next);
-        }
-        input.backup(1);
-    }
-    
-    protected void nextWhileMatchesRegExp(String pattern) {
-        StringBuilder s = new StringBuilder();
-        char next;
-        while (((next = (char) input.read()) != LexerInput.EOF) && Pattern.matches(pattern, new Character(next).toString())) {
-            s.append(next);
-        }
-        input.backup(1);
-    }
+	protected TSTokenId readRegexp() {
+		nextUntilUnescaped('/');
+		nextWhileMatchesRegExp("[gi]");
+		return TSTokenId.TS_REGEXP;
+	}
 
-    protected TSTokenId readNumber() {
-        nextWhileDigit();
-        return TSTokenId.TS_NUMBER;
-    }
+	protected String nextWhileWordChar() {
+		char next;
+		while (((next = (char) input.read()) != LexerInput.EOF) && isWordChar(new Character(next).toString()) && next != '\n') {
+		}
+		input.backup(1);
+		
+		return input.readText().toString();
+	}
 
-    protected TSTokenId readHexNumber() {
-        input.read();
-        // skip the 'x'
-        nextWhileHexDigit();
+	protected void nextWhileMatchesRegExp(String pattern) {
+		StringBuilder s = new StringBuilder();
+		char next;
+		while (((next = (char) input.read()) != LexerInput.EOF) && Pattern.matches(pattern, new Character(next).toString())) {
+			s.append(next);
+		}
+		input.backup(1);
+	}
 
-        return TSTokenId.TS_NUMBER;
-    }
+	protected TSTokenId readNumber() {
+		nextWhileDigit();
+		return TSTokenId.TS_NUMBER;
+	}
 
-    private void nextWhileOperatorChar() {
-        StringBuilder s = new StringBuilder();
-        char next;
-        while (((next = (char) input.read()) != LexerInput.EOF) && isOperatorChar(new Character(next).toString())) {
-            s.append(next);
-        }
-        input.backup(1);
-    }
+	protected TSTokenId readHexNumber() {
+		input.read();
+		// skip the 'x'
+		nextWhileHexDigit();
 
-    private void  nextWhileDigit() {
-        StringBuilder s = new StringBuilder();
-        char next;
-        while (((next = (char) input.read()) != LexerInput.EOF) && isDigit(new Character(next).toString())) {
-            s.append(next);
-        }
-        input.backup(1);
-    }
+		return TSTokenId.TS_NUMBER;
+	}
 
-    protected void nextWhileHexDigit() {
-        StringBuilder s = new StringBuilder();
-        char next;
-        while (((next = (char) input.read()) != LexerInput.EOF) && isHexDigit(new Character(next).toString())) {
-             s.append(next);
-        }
-    }
+	private void nextWhileOperatorChar() {
+		StringBuilder s = new StringBuilder();
+		char next;
+		while (((next = (char) input.read()) != LexerInput.EOF) && isOperatorChar(new Character(next).toString())) {
+			s.append(next);
+		}
+		input.backup(1);
+	}
 
-    protected void nextWhileWhiteSpace() {
-        StringBuilder s = new StringBuilder();
-        char next;
-        while (((next = (char) input.read()) != LexerInput.EOF) && isWhiteSpace(next)) {
-             s.append(next);
-        }
-        input.backup(1);
-    }
+	private void nextWhileDigit() {
+		StringBuilder s = new StringBuilder();
+		char next;
+		while (((next = (char) input.read()) != LexerInput.EOF) && isDigit(new Character(next).toString())) {
+			s.append(next);
+		}
+		input.backup(1);
+	}
 
-    protected void nextUntilUnescaped(char end) {
-        boolean escaped = false;
-        char next = (char) input.read();
-        while (((next = (char) input.read()) != LexerInput.EOF) && next != '\n') {
-            if (next == end && !escaped) {
-                break;
-            }
-            escaped = next == '\\';
-        }
-    }
+	protected void nextWhileHexDigit() {
+		StringBuilder s = new StringBuilder();
+		char next;
+		while (((next = (char) input.read()) != LexerInput.EOF) && isHexDigit(new Character(next).toString())) {
+			s.append(next);
+		}
+	}
 
-    protected TSTokenId readMultilineComment(char start) {
-        this.inComment = true;
-        boolean maybeEnd = (start == '*');
-        while (true) {
-            char next = (char) input.read();
-            if (next == '\n') {
-                break;
-            }
+	protected void nextWhileWhiteSpace() {
+		StringBuilder s = new StringBuilder();
+		char next;
+		while (((next = (char) input.read()) != LexerInput.EOF) && isWhiteSpace(next)) {
+			s.append(next);
+		}
+		input.backup(1);
+	}
 
-            if (next == '/' && maybeEnd) {
-                this.inComment = false;
-                break;
-            }
-            maybeEnd = (next == '*');
-        }
+	protected void nextUntilUnescaped(char end) {
+		boolean escaped = false;
+		char next = (char) input.read();
+		while (((next = (char) input.read()) != LexerInput.EOF) && next != '\n') {
+			if (next == end && !escaped) {
+				break;
+			}
+			escaped = next == '\\';
+		}
+	}
 
-        return TSTokenId.TS_COMMENT;
-    }
+	protected TSTokenId readMultilineComment(char start) {
+		this.inComment = true;
+		boolean maybeEnd = (start == '*');
+		while (true) {
+			char next = (char) input.read();
+			if (next == '\n') {
+				break;
+			}
 
-    protected boolean isOperatorChar(String input) {
-        return Pattern.matches("[\\+\\-\\*\\&\\%\\/=<>!\\?]", input);
-    }
+			if (next == '/' && maybeEnd) {
+				this.inComment = false;
+				break;
+			}
+			maybeEnd = (next == '*');
+		}
 
-    protected boolean isDigit(String input) {
-        return Pattern.matches("[0-9]", input);
-    }
+		return TSTokenId.TS_COMMENT;
+	}
 
-    protected boolean isHexDigit(String input) {
-        return Pattern.matches("[0-9A-Fa-f]", input);
-    }
+	protected boolean isOperatorChar(String input) {
+		return Pattern.matches("[\\+\\-\\*\\&\\%\\/=<>!\\?]", input);
+	}
 
-    protected boolean isWordChar(String input) {
-        return Pattern.matches("[\\w\\$_{}]", input);
-    }
+	protected boolean isDigit(String input) {
+		return Pattern.matches("[0-9]", input);
+	}
 
-    protected boolean isWhiteSpace(char ch) {
-        return ch != '\n' && (ch == ' ' || Pattern.matches("\\s", new Character(ch).toString()));
-    }
+	protected boolean isHexDigit(String input) {
+		return Pattern.matches("[0-9A-Fa-f]", input);
+	}
 
-    /**
-     * @return the readLength
-     */
-    public int getReadLength() {
-        return readLength;
-    }
+	protected boolean isWordChar(String input) {
+		return Pattern.matches("[\\w\\$_{}]", input);
+	}
+
+	protected boolean isWhiteSpace(char ch) {
+		return ch != '\n' && (ch == ' ' || Pattern.matches("\\s", new Character(ch).toString()));
+	}
+
+	/**
+	 * @return the readLength
+	 */
+	public int getReadLength() {
+		return readLength;
+	}
 }
