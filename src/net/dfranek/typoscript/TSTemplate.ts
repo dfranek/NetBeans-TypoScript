0917ad2b981b74906f5e1534eadd2eda477c3d70
Daniel Franek <daniel@dfrank.net>
2011-10-15 14:41:00 +0200

    changed template

---
+++ src/net/dfranek/typoscript/TSTemplate.ts
@@ -0,0 +1,13 @@
+#######################################################################
+# Comment
+#######################################################################
+page = PAGE
+page.config.SUBTEMPLATE = {$SUBTEMPLATE}
+
+headerimg.file {
+	XY = [10.w]+2,20
+}
+
+[globalVar=TSFE:type = 0]
+	page.typeNum = 0
+[global]
