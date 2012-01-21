63f77f89e9e88baa1784060d4a6e8315d5d20e5a
Daniel Franek <daniel@dfranek.net>
2012-01-21 19:28:33 +0100

    implemented some tests

--- src/net/dfranek/typoscript/TSDataObject.java
+++ src/net/dfranek/typoscript/TSDataObject.java
@@ -44,11 +44,11 @@ import org.openide.loaders.DataNode;
 import org.openide.loaders.DataObjectExistsException;
 import org.openide.loaders.MultiDataObject;
 import org.openide.loaders.MultiFileLoader;
+import org.openide.nodes.Children;
 import org.openide.nodes.CookieSet;
 import org.openide.nodes.Node;
-import org.openide.nodes.Children;
-import org.openide.util.Lookup;
 import org.openide.text.DataEditorSupport;
+import org.openide.util.Lookup;
 /**
  *
  * @author Daniel Franek
